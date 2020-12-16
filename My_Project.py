b5b328fed53f32629cb5e7b6382a44c897417f36
brynnceschan <71729938+brynnceschan@users.noreply.github.com>
2020-12-16 18:45:09 -0500

    Update My_Project.py
    Updated menu to show the list and options before entering new item.
    Figured out how to remove times from the time list.

--- My_Project.py
+++ My_Project.py
@@ -10,15 +10,17 @@ def get_my_schedule():
     item_counter = 0
     item = ""
     # Creating a loop that will continue to fill in the lists until a user wants it to stop
-    item = str(input("What is the title of item 1 on your to-do list? (Input 0 when you are done, 1 to edit the list): "))
+    item = str(input("What is the title of item 1 on your to-do list?: "))
     while(item != "0"):
         if(item == "1"):
             edit_schedule()
-            item = str(input("What is the title of item " + str(item_counter + 1) + " on your to-do list? (Input 0 when you are done, 1 to edit the list): "))
+            show_list()
+            item = str(input("Input 0 to exit, Input 1 to check off an item, or Input the title of item " + str(item_counter + 1) + " to continue adding to your to-do list: "))
         else:
             item_counter += 1
             add_item_to_schedule(item)
-            item = str(input("What is the title of item " + str(item_counter + 1) + " on your to-do list? (Input 0 when you are done, 1 to edit the list): "))
+            show_list()
+            item = str(input("Input 0 to exit, Input 1 to check off an item, or Input the title of item " + str(item_counter + 1) + " to continue adding to your to-do list: "))
     return item_counter
 
 # Function to add items to each list
@@ -29,6 +31,13 @@ def add_item_to_schedule(item_name):
     time = str(input("How long will '" + item_name + "' take to complete in minutes?: "))
     to_do_time.append(time)
 
+# Function that just shows both lists
+def show_list():
+    print("To-do List:")
+    print(to_do_list)
+    print("To-do Times:")
+    print(to_do_time)
+
 # Function to check off items
 def edit_schedule():
     print("Here is your to-do list:")
@@ -36,12 +45,21 @@ def edit_schedule():
     item_select = str(input("Which item would you like to check off?: "))
     # Assume the item is not in the list until it is found in the following for loop
     item_found = False
+    # item_location will be the location of the item in the to do list, so that we can remove the corresponding time from the time list
+    item_location = 0
+    # Beginning the counter at 0, will increase by 1 every time we go through the loop
+    counter = 0
+
     # Checks to see if the user input is actually in the to-do list
     for item in to_do_list:
         if(item == item_select):
             item_found = True
+            item_location = counter
+        counter = counter + 1
     if(item_found):
-        to_do_list.remove(item)
+        # Remove item from to do list & Pop corresponding time from time list
+        to_do_list.remove(item_select)
+        to_do_time.pop(item_location)
         print("Item successfully removed.")
     # If the input is not in the to-do list, send an error message and let them try again
     else:
@@ -61,7 +79,7 @@ def edit_schedule():
 # Prints to Show that the function is working
 number_of_items = get_my_schedule()
 print("Number of things to do today: " + str(number_of_items))
-print("To-do List:")
+print("Final To-do List:")
 print(to_do_list)
-print("To-do Times:")
+print("Final To-do Times:")
 print(to_do_time)
